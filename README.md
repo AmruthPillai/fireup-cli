ce027fe0cbea3ba1140fdb504544a22ec6395ca3
Amruth Pillai <im.amruth@gmail.com>
2020-04-07 22:02:50 +0530

    working with options

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# fireup-cli
+# FireUp CLI 🔥
 
-Upload anything right from your command line to your own private Firebase Storage bucket.
+Upload anything, right from your command line.
 
 [![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
 [![Version](https://img.shields.io/npm/v/fireup-cli.svg)](https://npmjs.org/package/fireup-cli)
@@ -11,6 +11,7 @@ Upload anything right from your command line to your own private Firebase Storag
 
 - [Usage](#usage)
 - [Commands](#commands)
+
 <!-- tocstop -->
 
 # Usage
@@ -18,11 +19,11 @@ Upload anything right from your command line to your own private Firebase Storag
 <!-- usage -->
 
 ```sh-session
-$ npm install -g fireup-cli
-$ fireup COMMAND
-running command...
+$ npm i -g fireup-cli
+
 $ fireup (-v|--version|version)
 fireup-cli/0.0.1 darwin-x64 node-v13.12.0
+
 $ fireup --help [COMMAND]
 USAGE
   $ fireup COMMAND
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
+import * as ora from 'ora';
 import * as chalk from 'chalk';
 import * as admin from 'firebase-admin';
 import * as clipboardy from 'clipboardy';
 import { Command, flags } from '@oclif/command';
 
-admin.initializeApp({
-  credential: admin.credential.cert(String(process.env.FIREUP_GSA)),
-  storageBucket: 'fireup-cli.appspot.com',
-});
-const bucket = admin.storage().bucket();
-
 class FireUpCli extends Command {
-  static description = 'describe the command here';
+  static description = 'Upload anything, right from your command line.';
 
   static flags = {
     file: flags.string({
       char: 'f',
-      description: 'The file you want to upload',
+      description: 'path of the file you want to upload',
     }),
     public: flags.boolean({
       char: 'p',
       default: false,
-      description: 'Do you want the file to be publicly accesible?',
+      description: 'make the file publicly-accessible',
+      exclusive: ['link'],
+    }),
+    link: flags.boolean({
+      char: 'l',
+      default: false,
+      description: 'if private, generate a temporarily downloadable link',
+      exclusive: ['public'],
     }),
     version: flags.version({ char: 'v' }),
     help: flags.help({ char: 'h' }),
@@ -29,20 +31,51 @@ class FireUpCli extends Command {
   static args = [{ name: 'file' }];
 
   async run(): Promise<void> {
+    admin.initializeApp({
+      credential: admin.credential.cert(String(process.env.FIREUP_GSA)),
+      storageBucket: 'fireup-cli.appspot.com',
+    });
+    const bucket = admin.storage().bucket();
+
     const { args, flags } = this.parse(FireUpCli);
     const filePath = flags.file || args.file;
+
+    const spinner = ora({
+      text: 'Uploading file...',
+      spinner: 'triangle',
+    }).start();
+
     const response = await bucket.upload(filePath, {
       public: flags.public,
     });
-    const publicUrl = response[0].metadata.mediaLink;
-    await clipboardy.write(publicUrl);
+
+    spinner.stop();
 
     this.log(chalk.blue('Your file has been uploaded successfully!\n'));
 
     if (flags.public) {
+      const publicUrl = response[0].metadata.mediaLink;
+      await clipboardy.write(publicUrl);
+
       this.log(
         chalk.blue('Public URL:'),
         chalk.bold.underline(publicUrl),
+        chalk.yellow('(copied to clipboard)'),
+      );
+    } else if (flags.link) {
+      const signedUrlResponse = await response[0].getSignedUrl({
+        action: 'read',
+        expires: new Date(new Date().getTime() + 60 * 60 * 4 * 1000),
+      });
+      const privateUrl = signedUrlResponse[0];
+      await clipboardy.write(privateUrl);
+
+      this.log(
+        chalk.blue.bold('This link is valid only for the next 4 hours.'),
+      );
+      this.log(
+        chalk.blue('Private URL:'),
+        chalk.bold.underline(privateUrl),
         chalk.yellow('(copied to clipboard)'),
       );
     }
