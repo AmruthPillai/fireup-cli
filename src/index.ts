fead5eb6cb414ab3b9838454299976286a7c95c8
Amruth Pillai <im.amruth@gmail.com>
2020-04-07 21:11:02 +0530

    uploading file, private/public

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
-import * as fs from 'fs';
-import * as util from 'util';
+import * as chalk from 'chalk';
+import * as admin from 'firebase-admin';
+import * as clipboardy from 'clipboardy';
 import { Command, flags } from '@oclif/command';
 
-const readFile = util.promisify(fs.readFile);
+admin.initializeApp({
+  credential: admin.credential.cert(String(process.env.FIREUP_GSA)),
+  storageBucket: 'fireup-cli.appspot.com',
+});
+const bucket = admin.storage().bucket();
 
-class FireupCli extends Command {
+class FireUpCli extends Command {
   static description = 'describe the command here';
 
   static flags = {
+    file: flags.string({
+      char: 'f',
+      description: 'The file you want to upload',
+    }),
+    public: flags.boolean({
+      char: 'p',
+      default: false,
+      description: 'Do you want the file to be publicly accesible?',
+    }),
     version: flags.version({ char: 'v' }),
     help: flags.help({ char: 'h' }),
   };
@@ -15,11 +29,24 @@ class FireupCli extends Command {
   static args = [{ name: 'file' }];
 
   async run(): Promise<void> {
-    const { args, flags } = this.parse(FireupCli);
+    const { args, flags } = this.parse(FireUpCli);
+    const filePath = flags.file || args.file;
+    const response = await bucket.upload(filePath, {
+      public: flags.public,
+    });
+    const publicUrl = response[0].metadata.mediaLink;
+    await clipboardy.write(publicUrl);
 
-    const file = await readFile(args.file);
-    console.log(file);
+    this.log(chalk.blue('Your file has been uploaded successfully!\n'));
+
+    if (flags.public) {
+      this.log(
+        chalk.blue('Public URL:'),
+        chalk.bold.underline(publicUrl),
+        chalk.yellow('(copied to clipboard)'),
+      );
+    }
   }
 }
 
-export = FireupCli;
+export = FireUpCli;
